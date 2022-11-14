442a53fab409733f915ecd771567ae27d3908eca
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2022-11-14 13:58:48 +0000

    Version 1.0

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
-
 function opentab(tabname){
   for(tablink of tablinks){
     tablink.classList.remove("active-link");
@@ -11,3 +10,12 @@ function opentab(tabname){
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab")
 }
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+
